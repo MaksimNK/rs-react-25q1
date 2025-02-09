53a3a64d09581032fd98a8a4ebed0f7e48a73e6e
MaksimNK <maksmozir@gmail.com>
2025-02-10 00:47:37 +0300

    fix: update to fc

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,19 +1,18 @@
 import { FC } from 'react';
 import Item from './Item';
 import { IItem } from '../utils/api';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 interface IListItemProps {
   items: IItem[];
-};
+}
 
 export const ListItem: FC<IListItemProps> = ({ items }) => {
   const [searchParams, setSearchParams] = useSearchParams();
-  
 
   const handleItemClick = (event: React.MouseEvent, id: string) => {
     event.stopPropagation();
-    searchParams.set("details", id);
+    searchParams.set('details', id);
     setSearchParams(searchParams);
   };
 
@@ -21,7 +20,7 @@ export const ListItem: FC<IListItemProps> = ({ items }) => {
     <div>
       <ul>
         {items.map((item) => {
-          const id = item.url.split("/").filter(Boolean).pop()!;
+          const id = item.url.split('/').filter(Boolean).pop() ?? '';
           return (
             <li key={item.url} onClick={(event) => handleItemClick(event, id)}>
               <Item data={item} />
@@ -31,7 +30,6 @@ export const ListItem: FC<IListItemProps> = ({ items }) => {
       </ul>
     </div>
   );
-}
-
+};
 
 export default ListItem;
